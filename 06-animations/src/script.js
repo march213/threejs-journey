bdbf27097cf591a61c9548da596611c2526af9f0
march213 <jane.molodetskaya@gmail.com>
2023-02-18 13:44:41 -0500

    animate camera object

--- 06-animations/src/script.js
+++ 06-animations/src/script.js
@@ -37,7 +37,12 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
+  // mesh.position.x = Math.sin(elapsedTime);
+  // mesh.position.y = Math.cos(elapsedTime);
+  camera.position.x = Math.sin(elapsedTime);
+  camera.position.y = Math.cos(elapsedTime);
+  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera);
