7984938b7347fb52f35fed22b8ed3ef3d2790fa7
march213 <jane.molodetskaya@gmail.com>
2023-03-26 19:41:50 -0400

    create triangle using BufferGeometry

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -11,8 +11,30 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 3, 3, 3);
+const geometry = new THREE.BufferGeometry();
+const positionsArray = new Float32Array(9);
+
+positionsArray[0] = 0;
+positionsArray[1] = 0;
+positionsArray[2] = 0;
+
+positionsArray[3] = 0;
+positionsArray[4] = 1;
+positionsArray[5] = 0;
+
+positionsArray[6] = 1;
+positionsArray[7] = 0;
+positionsArray[8] = 0;
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+
+geometry.setAttribute('position', positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
