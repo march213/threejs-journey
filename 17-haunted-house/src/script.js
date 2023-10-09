58be4cf7f3a0de3236e807f00fdc0c0aaf9cf276
march213 <jane.molodetskaya@gmail.com>
2023-10-09 18:05:05 -0400

    add door; bushes and graves

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -33,7 +33,7 @@ const walls = new THREE.Mesh(
   new THREE.BoxGeometry(4, 2.5, 4),
   new THREE.MeshStandardMaterial({
     color: '#ac8e82',
-  })
+  }),
 );
 walls.position.y = 2.5 / 2;
 house.add(walls);
@@ -43,11 +43,66 @@ const roof = new THREE.Mesh(
   new THREE.ConeGeometry(3.5, 1, 4),
   new THREE.MeshStandardMaterial({
     color: '#b35f45',
-  })
+  }),
 );
 roof.rotation.y = Math.PI * 0.25;
 roof.position.y = 2.5 + 0.5;
 house.add(roof);
+
+// Door
+
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(1.5, 2),
+  new THREE.MeshStandardMaterial({
+    color: '#aa7b7b',
+  }),
+);
+door.position.y = 1;
+door.position.z = 2 + 0.01;
+house.add(door);
+
+// Bushes
+
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' });
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+
+// Graves
+const graves = new THREE.Group();
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial({ color: '#b2b6b1' });
+
+for (let i = 0; i < 50; i++) {
+  const angle = Math.random() * Math.PI * 2;
+  const radius = 3.5 + Math.random() * 6;
+  const x = Math.sin(angle) * radius;
+  const z = Math.cos(angle) * radius;
+
+  const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+  grave.position.set(x, 0.3, z);
+  grave.rotation.y = (Math.random() - 0.5) * 0.4;
+  grave.rotation.z = (Math.random() - 0.5) * 0.4;
+  graves.add(grave);
+}
+
+scene.add(graves);
 
 // Floor
 const floor = new THREE.Mesh(new THREE.PlaneGeometry(20, 20), new THREE.MeshStandardMaterial({ color: '#a9c388' }));
