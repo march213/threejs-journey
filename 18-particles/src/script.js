3ea34d3529e130332a71b0fe3fa7469f21a159c6
march213 <jane.molodetskaya@gmail.com>
2023-12-25 12:02:49 -0500

    comment animation

--- 18-particles/src/script.js
+++ 18-particles/src/script.js
@@ -115,13 +115,13 @@ const tick = () => {
   const elapsedTime = clock.getElapsedTime();
 
   // particles.rotation.y = elapsedTime * 0.2;
-  for (let i = 0; i < particlesCount; i++) {
-    const i3 = i * 3;
-    const x = customParticlesGeometry.attributes.position.array[i3];
-    customParticlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
-  }
+  // for (let i = 0; i < particlesCount; i++) {
+  //   const i3 = i * 3;
+  //   const x = customParticlesGeometry.attributes.position.array[i3];
+  //   customParticlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
+  // }
 
-  customParticlesGeometry.attributes.position.needsUpdate = true;
+  // customParticlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();
