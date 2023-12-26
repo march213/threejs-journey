00e6ace64534609e2e09b10db391c080b284b56b
march213 <jane.molodetskaya@gmail.com>
2023-12-25 21:44:31 -0500

    parameterize galaxy generator

--- 18-galaxy-generator/src/script.js
+++ 18-galaxy-generator/src/script.js
@@ -19,31 +19,72 @@ const scene = new THREE.Scene();
  */
 
 const parameters = {};
-parameters.count = 1000;
+parameters.count = 100000;
+parameters.size = 0.01;
+parameters.radius = 5;
+parameters.branches = 3;
+parameters.spin = 1;
+parameters.randomness = 0.2;
+
+let geometry = null;
+let material = null;
+let particles = null;
 
 const generateGalaxy = () => {
-  const geometry = new THREE.BufferGeometry();
-  const material = new THREE.PointsMaterial();
+  // Destroy old galaxy
+  if (particles !== null) {
+    geometry.dispose();
+    material.dispose();
+    scene.remove(particles);
+  }
 
-  material.size = 0.005;
-  material.sizeAttenuation = true;
-
+  /**
+   * Geometry
+   */
+  geometry = new THREE.BufferGeometry();
   const positions = new Float32Array(parameters.count * 3);
 
   for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3;
-    positions[i3] = (Math.random() - 0.5) * 3;
-    positions[i3 + 1] = (Math.random() - 0.5) * 3;
-    positions[i3 + 2] = (Math.random() - 0.5) * 3;
+
+    const radius = Math.random() * parameters.radius;
+    const spinAngle = radius * parameters.spin;
+    const branchAngle = ((i % parameters.branches) / parameters.branches) * Math.PI * 2;
+
+    const randomX = (Math.random() - 0.5) * parameters.randomness;
+    const randomY = (Math.random() - 0.5) * parameters.randomness;
+    const randomZ = (Math.random() - 0.5) * parameters.randomness;
+
+    positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX; // x
+    positions[i3 + 1] = randomY; // y
+    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ; // z
   }
 
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
 
-  // Points
-  const particles = new THREE.Points(geometry, material);
+  /**
+   * Material
+   */
+  material = new THREE.PointsMaterial();
+  material.size = parameters.size;
+  material.sizeAttenuation = true;
+  material.depthWrite = false;
+  material.blending = THREE.AdditiveBlending;
+
+  /**
+   * Points
+   */
+  particles = new THREE.Points(geometry, material);
   scene.add(particles);
 };
 generateGalaxy();
+
+gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy);
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy);
+gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy);
+gui.add(parameters, 'spin').min(-5).max(5).step(0.001).onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy);
 
 /**
  * Sizes
