f12b9771ff7312578122f824fd85dc62499f2765
march213 <jane.molodetskaya@gmail.com>
2023-12-26 12:02:03 -0500

    set default material to the world

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -40,14 +40,14 @@ const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
 // Materials
-const concreteMaterial = new CANNON.Material('concrete');
-const plasticMaterial = new CANNON.Material('plastic');
+const defaultMaterial = new CANNON.Material('default');
 
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(concreteMaterial, plasticMaterial, {
+const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
   friction: 0.1,
   restitution: 0.7,
 });
-world.addContactMaterial(concretePlasticContactMaterial);
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
 
 // Sphere
 const shphereShape = new CANNON.Sphere(0.5);
@@ -55,7 +55,6 @@ const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: shphereShape,
-  material: plasticMaterial,
 });
 world.addBody(sphereBody);
 
@@ -65,7 +64,6 @@ const floorBody = new CANNON.Body();
 floorBody.mass = 0;
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
-floorBody.material = concreteMaterial;
 world.addBody(floorBody);
 
 /**
