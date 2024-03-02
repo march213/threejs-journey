2b95f970e35a1c6c947a838846ff05ef79e9f0b3
march213 <jane.molodetskaya@gmail.com>
2024-03-02 16:42:46 -0500

    setup camera

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,8 +1,15 @@
+import * as THREE from "three";
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
+import Camera from "./Camera";
+
+let instance = null;
 
 export default class Experience {
   constructor(canvas) {
+    if (instance) return instance;
+    instance = this;
+
     window.experience = this;
 
     // Options
@@ -10,12 +17,16 @@ export default class Experience {
 
     this.sizes = new Sizes();
     this.time = new Time();
+    this.scene = new THREE.Scene();
+    this.camera = new Camera();
 
     this.sizes.on("resize", this.resize);
     this.time.on("tick", this.update);
   }
 
-  resize = () => {};
+  resize = () => {
+    this.camera.resize();
+  };
 
   update = () => {};
 }
