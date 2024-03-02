dea838aef97a3e5b3b0fdf2eb9d3873f83618b15
march213 <jane.molodetskaya@gmail.com>
2024-03-01 23:42:10 -0500

    add time class and invoke it

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,4 +1,5 @@
 import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
 
 export default class Experience {
   constructor(canvas) {
@@ -8,9 +9,13 @@ export default class Experience {
     this.canvas = canvas;
 
     this.sizes = new Sizes();
+    this.time = new Time();
 
     this.sizes.on("resize", this.resize);
+    this.time.on("tick", this.update);
   }
 
   resize = () => {};
+
+  update = () => {};
 }
