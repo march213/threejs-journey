576938a45460f3cc6e44b9612dbcc020babb10b8
march213 <jane.molodetskaya@gmail.com>
2024-03-21 15:50:25 -0400

    add destroy method

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -44,4 +44,36 @@ export default class Experience {
     this.world.update();
     this.renderer.update();
   };
+
+  // Consider to implement a destroy method in each class, rather than calling it here
+  destroy = () => {
+    this.sizes.off("resize");
+    this.time.off("tick");
+
+    // Traverse the whole scene
+    this.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.geometry.dispose();
+
+        for (const key in child.material) {
+          const value = child.material[key];
+
+          // Test if there is a dispose method
+          if (value && typeof value.dispose === "function") {
+            value.dispose();
+          }
+        }
+      }
+    });
+
+    this.camera.orbitControls.dispose();
+    this.renderer.instance.dispose();
+
+    if (this.debug.active) {
+      this.debug.gui.destroy();
+    }
+
+    // If using post-processing, you'll need to dispose of the EffectComposer,
+    // its WebGLRenderTarget and any potential passes you are using
+  };
 }
