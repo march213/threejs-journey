c1ef1385318e4eeccd0c3b6d0f32d8e3de371726
march213 <jane.molodetskaya@gmail.com>
2024-03-22 18:49:59 -0400

    play with randomness in shader

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -3,11 +3,16 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
+
+varying float vRandom;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+    modelPosition.z += sin(aRandom * 5.0) * 0.1;
+    vRandom = aRandom;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
