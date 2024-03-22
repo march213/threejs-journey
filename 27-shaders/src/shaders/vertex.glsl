4412749a57d75e141b36e1d7bf51027815a2fc74
march213 <jane.molodetskaya@gmail.com>
2024-03-22 18:51:13 -0400

    reset changes in shaders

--- 27-shaders/src/shaders/vertex.glsl
+++ 27-shaders/src/shaders/vertex.glsl
@@ -3,16 +3,9 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
-attribute float aRandom;
-
-varying float vRandom;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-    // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
-    modelPosition.z += sin(aRandom * 5.0) * 0.1;
-    vRandom = aRandom;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
