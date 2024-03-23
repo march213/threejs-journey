8e27422421dfb9796e7c7976eddca1882386f257
march213 <jane.molodetskaya@gmail.com>
2024-03-22 20:55:16 -0400

    use texture in the shader and animate it

--- 27-shaders/src/script.js
+++ 27-shaders/src/script.js
@@ -20,6 +20,7 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const flagTexture = textureLoader.load("textures/flag-french.jpg");
 
 /**
  * Test mesh
@@ -40,10 +41,28 @@ geometry.setAttribute("aRandom", new THREE.BufferAttribute(randoms, 1));
 const material = new THREE.RawShaderMaterial({
   vertexShader: vertex,
   fragmentShader: fragment,
+  uniforms: {
+    uFrequency: {
+      value: new THREE.Vector2(10, 5),
+    },
+    uTime: {
+      value: 0,
+    },
+    uColor: {
+      value: new THREE.Color("orange"),
+    },
+    uTexture: {
+      value: flagTexture,
+    },
+  },
 });
+
+gui.add(material.uniforms.uFrequency.value, "x").min(0).max(20).step(0.01).name("frequencyX");
+gui.add(material.uniforms.uFrequency.value, "y").min(0).max(20).step(0.01).name("frequencyY");
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material);
+mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 /**
@@ -96,6 +115,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update material
+  material.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
