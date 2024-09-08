3329cf0a3fe1354638f798983564fd5dbd05c53e
march213 <jane.molodetskaya@gmail.com>
2024-09-08 13:56:39 -0400

    waved circle

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -184,10 +184,33 @@ void main() {
     // float strength = angle;
 
     // Pattern #41
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // float strength = angle;
+
+    // Pattern #42
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // angle *= 30.0;
+    // angle = mod(angle, 1.0);
+    // float strength = angle;
+
+    // Pattern #43
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+    // angle /= PI * 2.0;
+    // angle += 0.5;
+    // float strength = sin(angle * 100.0);
+
+    // Pattern #44
     float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
     angle /= PI * 2.0;
     angle += 0.5;
-    float strength = angle;
+    float sinusoid = sin(angle * 100.0);
+
+    float radius = 0.25 + sinusoid * 0.02;
+    float strength = 1.0 - step(0.01, abs(distance(vUv, vec2(0.5)) - radius));
 
     gl_FragColor = vec4(vec3(strength), 1.0);
 }
