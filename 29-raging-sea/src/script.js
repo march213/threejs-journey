a3889b789512c75cb412bada6eedf5d019306e0d
march213 <jane.molodetskaya@gmail.com>
2024-10-12 21:47:20 -0400

    setup waves in shaders and animate them

--- 29-raging-sea/src/script.js
+++ 29-raging-sea/src/script.js
@@ -26,7 +26,40 @@ const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
 const waterMaterial = new THREE.ShaderMaterial({
   vertexShader: waterVertexShader,
   fragmentShader: waterFragmentShader,
+  uniforms: {
+    uBigWavesElevation: { value: 0.2 },
+    uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5) },
+    uTime: { value: 0 },
+    uBigWavesSpeed: { value: 0.75 },
+  },
 });
+
+// Debug
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uBigWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uBigWavesSpeed");
+
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequency.x");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uBigWavesFrequency.y");
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial);
@@ -83,6 +116,9 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update water
+  waterMaterial.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
--- 29-raging-sea/src/shaders/water/vertex.glsl
+++ 29-raging-sea/src/shaders/water/vertex.glsl
@@ -1,7 +1,16 @@
+uniform float uTime;
+uniform float uBigWavesElevation;
+uniform float uBigWavesSpeed;
+uniform vec2 uBigWavesFrequency;
+
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    modelPosition.y += sin(modelPosition.x);
+    // Elevation
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed)
+            * sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed)
+            * uBigWavesElevation;
+    modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
